02905776d814594d33d48ef0ac92af1888c2589d
Marcio <margeodev@gmail.com>
2023-08-05 20:50:54 -0300

    Creation of core and shared module.

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [    
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ]
+})
+export class CoreModule { }
