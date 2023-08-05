1d59f9c8c6ba52a4e44259c2d9fc27ddb92b72e2
Marcio <margeodev@gmail.com>
2023-08-05 20:11:53 -0300

    Creation of category-list component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
