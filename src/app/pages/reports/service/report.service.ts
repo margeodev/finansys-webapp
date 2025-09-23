a688aa99a147ac5cda9e3cb59f303d84e17a5526
marcio <margeodev@gmail.com>
2025-09-23 14:03:45 -0300

    ajuste no serviço de relatórios para montar a url via environment

--- src/app/pages/reports/service/report.service.ts
+++ src/app/pages/reports/service/report.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { ReportModel } from '../model/report.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
 
-  private apiUrl: string = "/api/v1/reports";
+  private apiUrl: string = `${environment.apiUrl}/api/v1/reports`;
 
   constructor(private http: HttpClient) { }
 
@@ -21,14 +22,13 @@ export class ReportService {
     );
   }
 
-  private jsonDataToReport(jsonData: any[]): ReportModel[]{    
+  private jsonDataToReport(jsonData: any[]): ReportModel[] {    
     const reports: ReportModel[] = [];
     jsonData.forEach(element => reports.push(element as ReportModel));    
     return reports;
   }
 
-  private handleError(error: any): Observable<any>{
+  private handleError(error: any): Observable<any> {
     return throwError(error);
   }
-
 }
